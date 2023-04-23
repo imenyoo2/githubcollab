38e20f25ecc5b8346f5a0cc27ec51185a518128c
ayoub <ayoubaitelaouad@gmail.com>
2023-04-23 16:46:09 +0100

    initial commit

---
+++ .ccls-cache/@home@nyly@personal@alxSE@month2@githubcollab/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	printint(1938383);
+	printhex(1938383);
+}
---
+++ main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+void printint(int n);
+void printhex(int n);
+#endif
