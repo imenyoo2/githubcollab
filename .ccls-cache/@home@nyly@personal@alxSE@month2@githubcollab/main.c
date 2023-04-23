bcf7ac91bfdb932deda724b208a679daccef30ed
ayoub <ayoubaitelaouad@gmail.com>
2023-04-23 16:48:45 +0100

    gitignore

--- .ccls-cache/@home@nyly@personal@alxSE@month2@githubcollab/main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-	printint(1938383);
-	printhex(1938383);
-}
--- .ccls-cache/@home@nyly@personal@alxSE@month2@githubcollab/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-void printint(int n);
-void printhex(int n);
-#endif
